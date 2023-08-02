0710283efcbce0c906d7a3d588556ae496c985b5
NIKHIL RANJAN KUMAR <88980648+niku-19@users.noreply.github.com>
2023-08-02 21:14:18 +0530

    initial commit

--- src/components/Card.js
+++ src/components/Card.js
@@ -9,36 +9,31 @@ const Card = () => {
   const onClickHandler = (e) => {
     setCalText(calText + e.target.innerText);
   };
-  const onClickHandler_1 = (e) => {
-    setCalText(0);
-  };
-  const onClickHandler_2 = (e) => {
-    setCalText("");
-  };
+
   const onClickHandler_3 = (e) => {
     if (calText.includes("+")) {
       const arr = calText.split("+");
-      setCalText(parseInt(arr[0]) + parseInt(arr[1]));
+      setCalText(Number(arr[0]) + Number(arr[1]));
     } else if (calText.includes("-")) {
       const arr = calText.split("-");
-      setCalText(parseInt(arr[0]) - parseInt(arr[1]));
-    } else if (calText.includes("*")) {
-      const arr = calText.split("*");
-      setCalText(parseInt(arr[0]) * parseInt(arr[1]));
-    } else if (calText.includes("/")) {
-      const arr = calText.split("/");
-      setCalText(parseInt(arr[0]) / parseInt(arr[1]));
+      setCalText(Number(arr[0]) - Number(arr[1]));
+    } else if (calText.includes("X")) {
+      const arr = calText.split("X");
+      setCalText(Number(arr[0]) * Number(arr[1]));
+    } else if (calText.includes("÷")) {
+      const arr = calText.split("÷");
+      setCalText(Number(arr[0]) / Number(arr[1]));
     } else if (calText.includes("%")) {
       const arr = calText.split("%");
-      setCalText(parseInt(arr[0]) % parseInt(arr[1]));
+      setCalText(Number(arr[0]) % Number(arr[1]));
     } else {
-      setCalText("Error");
+      setCalText(calText);
     }
   };
 
   return (
     <>
-      <h1 align="center">Personal Calculator</h1>
+      <div className={AppStyle.shadow}></div>
       <div className={AppStyle.conatiner}>
         <div className={AppStyle.card}>
           <div className={AppStyle.CalResult}>
@@ -49,27 +44,24 @@ const Card = () => {
               min="0"
               max={10}
             />
-            <hr />
           </div>
           <div className={AppStyle.cal__buttons}>
-            <button onClick={onClickHandler}>%</button>
-            <button onClick={onClickHandler_1}>CE</button>
-            <button onClick={onClickHandler_2}>C</button>
             <button onClick={onClickHandler}>7</button>
             <button onClick={onClickHandler}>8</button>
             <button onClick={onClickHandler}>9</button>
+            <button onClick={onClickHandler}>÷</button>
             <button onClick={onClickHandler}>4</button>
             <button onClick={onClickHandler}>5</button>
             <button onClick={onClickHandler}>6</button>
+            <button onClick={onClickHandler}>X</button>
             <button onClick={onClickHandler}>1</button>
             <button onClick={onClickHandler}>2</button>
             <button onClick={onClickHandler}>3</button>
-            <button onClick={onClickHandler}>0</button>
-            <button onClick={onClickHandler}>/</button>
-            <button onClick={onClickHandler}>*</button>
-            <button onClick={onClickHandler}>+</button>
             <button onClick={onClickHandler}>-</button>
-            <button onClick={onClickHandler_3}>=</button>
+            <button onClick={onClickHandler}>0</button>
+            <button onClick={onClickHandler}>.</button>
+            <button onClick={onClickHandler}>+</button>
+            <button onClick={onClickHandler_3}> = </button>
           </div>
         </div>
       </div>
